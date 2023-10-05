4effd01226fc776c4ba06bc700a4a3391563e9dd
matiasGutierrez <m.ignacio.guti@gmail.com>
2023-10-05 18:36:57 -0300

    Finalizado codigo y estructura, pasar a primera prueba en pi

--- script.py
+++ script.py
@@ -4,12 +4,11 @@ import time
 from subprocess import PIPE, Popen, STDOUT
 
 directorio = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'videos')
-
 capitulos_ya_vistos = []
 
 def obtenerCapituloRandom():
   carpetas = [nombre for nombre in os.listdir(directorio) if os.path.isdir(os.path.join(directorio, nombre))]
-  carpetas_filtradas = [nombre for nombre in carpetas if nombre != "comerciales"]
+  carpetas_filtradas = [nombre for nombre in carpetas if nombre != "01-comerciales"]
   carpeta_random = random.choice(carpetas_filtradas)
 
   ruta_carpeta = os.path.join(directorio, carpeta_random)
@@ -18,10 +17,10 @@ def obtenerCapituloRandom():
   return ruta_completa
 
 def obtenerComerciales():
-  carpeta_comerciales = directorio + "\\comerciales"
-  archivos = [nombre for nombre in os.listdir(carpeta_comerciales) if os.path.isfile(os.path.join(carpeta_comerciales, nombre))]
-  archivos_seleccionados = random.sample(archivos, 3)
-  return archivos_seleccionados
+  carpeta_comerciales = os.path.join(directorio, "01-comerciales")
+  comerciales = [(os.path.join(carpeta_comerciales, nombre)) for nombre in os.listdir(carpeta_comerciales) if os.path.isfile(os.path.join(carpeta_comerciales, nombre))]
+  comerciales_seleccionados = random.sample(comerciales, 3)
+  return comerciales_seleccionados
 
 def reproducirVideos():
   print("------- Revision --------")
@@ -34,17 +33,20 @@ def reproducirVideos():
     if capitulo not in capitulos_ya_vistos:
       encontrado = False
 
-  capitulos_ya_vistos.append(capitulo)
-  print(capitulos_ya_vistos)
-  return(capitulo)
-  
-# print(obtenerCapituloRandom())
-# print(obtenerComerciales())
+  playProcess = Popen(['omxplayer', '--no-osd', '--aspect-mode', 'fill', capitulo])
+  playProcess.wait()
 
-i=0
-while (i<3):
+  capitulos_ya_vistos.append(capitulo)
+
+  comerciales = obtenerComerciales()
+
+  for comercial in comerciales:
+    playProcess = Popen(['omxplayer', '--no-osd', '--aspect-mode', 'fill', comercial])
+    playProcess.wait()
+
+
+while (True):
   reproducirVideos()
-  i += 1
 
 # cmd = "nohup omxplayer -b -o hdmi "+"'"+ruta_carpeta+episodio+"' &"
 # os.system('killall omxplayer.bin')
