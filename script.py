f3f62588dd096aeb1fb4ee2c82cb62ffc99f555e
matiasGutierrez <m.ignacio.guti@gmail.com>
2023-10-06 17:43:58 -0300

    Agregar .gitkeep a para obtener estructura de carpetas

--- script.py
+++ script.py
@@ -5,16 +5,16 @@ from subprocess import PIPE, Popen, STDOUT
 directorio = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'videos')
 capitulos_ya_vistos = []
 
+#La funciona ingresara al archivo /canal/canal.txt y sacara dentro del archivo el nombre del canal actual
 def obtenerCanalActual():
-  #Obtener path del archivo txt donde esta el canal actual
   directorio_canal = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'canal', 'canal.txt')
   
-  #Leer archivo txt y guardar en una variable el canal actual
   with open(directorio_canal, 'r') as file:
       canal = file.read()
 
   return canal
 
+#La funciona elegira un capitulo al azar dentro de la carpeta del canal actual
 def obtenerCapituloRandom():
   canal = obtenerCanalActual()
   directorio_capitulos = os.path.join(directorio, canal)
@@ -25,14 +25,16 @@ def obtenerCapituloRandom():
 
   return ruta_completa
 
-def obtenerComerciales():
+#La funcion armara un arreglo de "N" comerciales al azar, donde "N" es elegido por el usuario
+def obtenerComerciales(n_comerciales):
   canal = obtenerCanalActual()
   carpeta_comerciales = os.path.join(directorio, "01-comerciales", canal)
   comerciales = [(os.path.join(carpeta_comerciales, nombre)) for nombre in os.listdir(carpeta_comerciales) if os.path.isfile(os.path.join(carpeta_comerciales, nombre))]
-  comerciales_seleccionados = random.sample(comerciales, 4)
+  comerciales_seleccionados = random.sample(comerciales, n_comerciales)
 
   return comerciales_seleccionados
 
+#La funcion reproducira un capitulo elegido al azar (siempre que no haya salido ya) y un set de "N" comerciales despues del capitulo
 def reproducirVideos():
   global capitulos_ya_vistos
   encontrado = True
@@ -42,14 +44,16 @@ def reproducirVideos():
     if capitulo not in capitulos_ya_vistos:
       encontrado = False
 
+  #Reproducir video
   playProcess = Popen(['omxplayer', '--no-osd', '--aspect-mode', 'fill', capitulo])
   playProcess.wait()
 
   capitulos_ya_vistos.append(capitulo)
 
-  comerciales = obtenerComerciales()
+  comerciales = obtenerComerciales(n_comerciales=3)
 
   for comercial in comerciales:
+    #Reproducir video
     playProcess = Popen(['omxplayer', '--no-osd', '--aspect-mode', 'fill', comercial])
     playProcess.wait()
 
