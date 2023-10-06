3b0f15ea12bb0ac3eb0120367dfe88e4b0eef3a9
matiasGutierrez <m.ignacio.guti@gmail.com>
2023-10-06 17:19:03 -0300

    Finalizada logica de seleccion de capitulos y comerciales del canal seleccionado

--- script.py
+++ script.py
@@ -2,31 +2,35 @@ import os
 import random 
 from subprocess import PIPE, Popen, STDOUT
 
-
-
 directorio = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'videos')
 capitulos_ya_vistos = []
 
 def obtenerCanalActual():
-  with open(archivo, 'r') as file:
-      file_content = file.read()
+  #Obtener path del archivo txt donde esta el canal actual
+  directorio_canal = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'canal', 'canal.txt')
+  
+  #Leer archivo txt y guardar en una variable el canal actual
+  with open(directorio_canal, 'r') as file:
+      canal = file.read()
 
-  print(file_content)
+  return canal
 
 def obtenerCapituloRandom():
-  carpetas = [nombre for nombre in os.listdir(directorio) if os.path.isdir(os.path.join(directorio, nombre))]
-  carpetas_filtradas = [nombre for nombre in carpetas if nombre != "01-comerciales"]
-  carpeta_random = random.choice(carpetas_filtradas)
-
-  ruta_carpeta = os.path.join(directorio, carpeta_random)
+  canal = obtenerCanalActual()
+  directorio_capitulos = os.path.join(directorio, canal)
+  carpetas = [nombre for nombre in os.listdir(directorio_capitulos) if os.path.isdir(os.path.join(directorio_capitulos, nombre))]
+  ruta_carpeta = os.path.join(directorio_capitulos, random.choice(carpetas))
   episodio = random.choice(os.listdir(ruta_carpeta))
   ruta_completa = os.path.join(ruta_carpeta, episodio)
+
   return ruta_completa
 
 def obtenerComerciales():
-  carpeta_comerciales = os.path.join(directorio, "01-comerciales")
+  canal = obtenerCanalActual()
+  carpeta_comerciales = os.path.join(directorio, "01-comerciales", canal)
   comerciales = [(os.path.join(carpeta_comerciales, nombre)) for nombre in os.listdir(carpeta_comerciales) if os.path.isfile(os.path.join(carpeta_comerciales, nombre))]
-  comerciales_seleccionados = random.sample(comerciales, 3)
+  comerciales_seleccionados = random.sample(comerciales, 4)
+
   return comerciales_seleccionados
 
 def reproducirVideos():
@@ -52,6 +56,7 @@ def reproducirVideos():
 
 while (True):
   reproducirVideos()
+
 
 # cmd = "nohup omxplayer -b -o hdmi "+"'"+ruta_carpeta+episodio+"' &"
 # os.system('killall omxplayer.bin')
--- todos.py
+++
@@ -1,6 +0,0 @@
-import os
-
-directorio_canal = directorio = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'canal', 'canal.txt')
-
-with open(directorio_canal, 'w') as file:
-    file.write('todos')    
